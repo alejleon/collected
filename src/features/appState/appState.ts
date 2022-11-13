db77c5e457628e1e408c4732b6654326a3f4c4ee
Alex Hernandez <aherna4@gmail.com>
2022-11-13 01:47:46 -0600

    linter and prettier setup

--- src/features/appState/appState.ts
+++ src/features/appState/appState.ts
@@ -1,7 +1,9 @@
-import create, { State } from "zustand";
+import create from 'zustand';
+import { State } from './types/appStateTypes';
 
 const useAppState = create((set) => ({
   bears: 0,
-  increaseBearPopulation: () => set((state ) => ({bears: state.bears + 1})),
-  killAllBears: () => set({bears: 0})
+  increaseBearPopulation: () =>
+    set((state: State) => ({ bears: state.bears + 1 })),
+  killAllBears: () => set({ bears: 0 }),
 }));
---
+++ src/features/appState/types/appStateTypes.ts
@@ -0,0 +1,5 @@
+export type AppState = {
+  bears: number;
+  increaseBearPopulation: () => void;
+  killAllBears: () => void;
+};
