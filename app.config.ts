3e330db486f71571932c339d4efeb1696bd77f9d
Alex Hernandez <aherna4@gmail.com>
2022-11-20 21:00:56 -0600

    mend

--- app.config.ts
+++ app.config.ts
@@ -1,38 +1,50 @@
-
 export default {
-  "expo": {
-    name: "collected",
-    slug: "collected",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/icon.png",
-    userInterfaceStyle: "light",
+  expo: {
+    name: 'collected',
+    slug: 'collected',
+    version: '1.0.0',
+    sdkVersion: '47.0.0',
+    jsEngine: 'hermes',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    backgroundColor: '#877473',
+    primaryColor: '#fcba03',
+    androidStatusBar: {
+      barStyle: 'light-content',
+      translucent: false,
+    },
+    androidNavigationBar: {
+      barStyle: 'light-content',
+      backgroundColor: '#877473',
+    },
+    scheme: 'collected',
     splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff"
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#FFFFFF',
     },
     updates: {
-      fallbackToCacheTimeout: 0
+      fallbackToCacheTimeout: 0,
     },
-    assetBundlePatterns: ["**/*"],
+    assetBundlePatterns: ['**/*'],
     ios: {
-      supportsTablet: false
+      supportsTablet: false,
     },
     android: {
       adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#FFFFFF"
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#FFFFFF',
       },
-      package: 'com.collectedapp.www'
+      package: 'com.collectedapp.www',
     },
     web: {
-      favicon: "./assets/favicon.png"
+      favicon: './assets/favicon.png',
     },
     extra: {
       eas: {
-        projectId: "6259e058-8794-4c8f-bf6d-b32f27d652bf"
-      }
-    }
-  }
-}
+        projectId: '6259e058-8794-4c8f-bf6d-b32f27d652bf',
+      },
+    },
+  },
+};
