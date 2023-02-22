dc53caeac86de6af6e560485865518c7b5d36749
Alex Hernandez <aherna4@gmail.com>
2023-02-22 00:43:34 -0600

    tinkering with pressable props, pressed, and typescript

--- src/features/app/components/Pressable.tsx
+++ src/features/app/components/Pressable.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { Pressable as RNPressable, PressableProps } from 'react-native';
 
-const Pressable = (props: PressableProps) => {
-  console.log('Pressable props: ', props);
+const Pressable = ({
+  children,
+  style,
+  // activeOpacity,
+  ...otherProps
+}: PressableProps) => {
+  // const _style: StyleProp<ViewStyle> = useCallback(
+  //   ({ pressed }: { pressed: boolean }) => [
+  //     { opacity: pressed ? activeOpacity : 1 },
+  //     style && style,
+  //   ],
+  //   [style]
+  // );
 
-  return <RNPressable {...props} />;
+  return (
+    <RNPressable style={style} {...otherProps}>
+      {children}
+    </RNPressable>
+  );
 };
 
 export default Pressable;
